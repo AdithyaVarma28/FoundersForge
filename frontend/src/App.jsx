03563fad28ed285869d8972e0752cad24bd6bbe5
AdithyaVarma28 <adithyavarma5678@gmail.com>
2026-04-16 11:57:38 +0530

    Update main App.jsx component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,62 @@
-import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import AppShell from './components/AppShell'
+import ProtectedRoute from './components/ProtectedRoute'
+import AuthPage from './pages/AuthPage'
+import ContributorDashboardPage from './pages/ContributorDashboardPage'
+import DashboardRedirectPage from './pages/DashboardRedirectPage'
+import DiscoverPage from './pages/DiscoverPage'
+import FounderDashboardPage from './pages/FounderDashboardPage'
+import HomePage from './pages/HomePage'
+import InvestorDashboardPage from './pages/InvestorDashboardPage'
+import NotFoundPage from './pages/NotFoundPage'
+import WorkspacePage from './pages/WorkspacePage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppShell />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/dashboard" element={<DashboardRedirectPage />} />
+          <Route
+            path="/founder"
+            element={
+              <ProtectedRoute allowedRole="Founder">
+                <FounderDashboardPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/contributor"
+            element={
+              <ProtectedRoute allowedRole="Contributor">
+                <ContributorDashboardPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/investor"
+            element={
+              <ProtectedRoute allowedRole="Investor">
+                <InvestorDashboardPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/discover" element={<DiscoverPage />} />
+          <Route
+            path="/workspace"
+            element={
+              <ProtectedRoute>
+                <WorkspacePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
